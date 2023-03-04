eca8e83cd4836a1d35f85a0bc5a257e56b0ffcfa
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-03-04 16:48:39 +0700

    BE | initial

---
+++ devops-springboot/src/test/java/com/ngochung/devopsspringboot/DevopsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngochung.devopsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
